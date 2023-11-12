78f137b4248d6fc71ce5b86364b0c20bd1df0526
Benxinm <yn03.zhang@gmail.com>
2023-11-12 23:16:58 +0800

    feat: unfavorite

---
+++ cmd/interaction/service/un_favorite_act.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"errors"
+	"github.com/benxinm/tiktok/cmd/interaction/dal/cache"
+	"github.com/benxinm/tiktok/cmd/interaction/dal/db"
+	"github.com/benxinm/tiktok/kitex_gen/interaction"
+	"gorm.io/gorm"
+)
+
+func (s *InteractionService) UnFavorite(req *interaction.FavoriteActionRequest, uid int64) error {
+	exist, err := cache.IsVideoFavoriteExist(s.ctx, req.VideoId, uid)
+	if err != nil {
+		return err
+	}
+	if !exist {
+		err := db.IsFavorited(s.ctx, uid, req.VideoId, 1)
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return err
+		}
+	}
+	_, err = cache.GetVideoLikeCount(s.ctx, req.VideoId)
+	if err != nil {
+		return err
+	}
+	if err := cache.ReduceVideoLikeCount(s.ctx, req.VideoId, uid); err != nil {
+		return err
+	}
+	return db.UpdateFavoriteStatus(s.ctx, uid, req.VideoId, 0)
+}
